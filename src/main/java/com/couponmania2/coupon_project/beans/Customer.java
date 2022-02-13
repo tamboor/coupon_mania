7909f6b6dd84d6b37ad5a3cbb1c1cb7ed4189e0a
ran-manor <raran937@gmail.com>
2022-02-13 12:28:27 +0200

    check

---
+++ src/main/java/com/couponmania2/coupon_project/beans/Customer.java
@@ -0,0 +1,32 @@
+package com.couponmania2.coupon_project.beans;
+
+import lombok.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.batch.BatchDataSource;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@Builder
+@Entity
+@AllArgsConstructor
+@Table(name= "customer")
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(nullable = false,length = 40)
+    private String firstName;
+    @Column(nullable = false,length = 40)
+    private String lastName;
+    @Column(nullable = false,length = 40)
+    private String email;
+    @Column(nullable = false,length = 40)
+    private String password;
+    @OneToMany (cascade = CascadeType.ALL,orphanRemoval = true,fetch = FetchType.EAGER)
+    @Singular
+    private List<Coupon> coupons = new ArrayList<>();
+}
