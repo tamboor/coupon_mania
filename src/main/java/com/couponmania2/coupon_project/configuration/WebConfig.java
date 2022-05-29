d0f7612ceda3c05505361bf5de160c5801ab3073
tamboor <nirkatz20exc@gmail.com>
2022-05-29 15:19:26 +0300

    removed viewcontrollers

--- src/main/java/com/couponmania2/coupon_project/configuration/WebConfig.java
+++ src/main/java/com/couponmania2/coupon_project/configuration/WebConfig.java
@@ -1,56 +1,24 @@
-package com.couponmania2.coupon_project.configuration;
+//package com.couponmania2.coupon_project.configuration;
+////
+////import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.cors.CorsConfiguration;
+//import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+//import org.springframework.web.filter.CorsFilter;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+//import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 //
-//import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-//@EnableWebMvc
-public class WebConfig implements WebMvcConfigurer {
-    @Override
-    public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/{spring:\\w+}")
-                .setViewName("forward:/");
-        registry.addViewController("/**/{spring:\\w+}")
-                .setViewName("forward:/");
-        registry.addViewController("/{spring:\\w+}/**{spring:?!(\\.js|\\.css)$}")
-                .setViewName("forward:/");    }
-
-
-    //
+//@Configuration
+////@EnableWebMvc
+//public class WebConfig implements WebMvcConfigurer {
 //    @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//        registry.addMapping("/**");
-//        registry.addMapping("*");
-//    }
-
-//    @Bean
-//    public CorsFilter corsFilter(){
-//
-//        //create new cors configuration...
-//        CorsConfiguration config = new CorsConfiguration();
-//        //allow to get credentials in cors
-//        config.setAllowCredentials(true);
-//        //allow to get from any ip/domain
-//        config.addAllowedOrigin("*");
-//        //allow to get any header
-//        config.addAllowedHeader("*");
-//        //tell which VERB is allowed
-//        config.addAllowedMethod("GET");
-//        config.addAllowedMethod("POST");
-//        config.addAllowedMethod("PUT");
-//        config.addAllowedMethod("DELETE");
-//        //allow to get any route -> localhost:8080/api/coupon-> /api/coupon and implement the config
-//        //create new url configuration for browsers
-//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-//        source.registerCorsConfiguration("/**",config);
-//        //return new configuration
-//        return new CorsFilter(source);
-//    }
-}
+//    public void addViewControllers(ViewControllerRegistry registry) {
+//        registry.addViewController("/{spring:\\w+}")
+//                .setViewName("forward:/");
+//        registry.addViewController("/**/{spring:\\w+}")
+//                .setViewName("forward:/");
+//        registry.addViewController("/{spring:\\w+}/**{spring:?!(\\.js|\\.css)$}")
+//                .setViewName("forward:/");    }
+//}
