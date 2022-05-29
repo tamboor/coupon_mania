4245ae2a7200825e5485c4c17ea6b1a8cf1c39b5
tamboor <nirkatz20exc@gmail.com>
2022-05-29 12:20:09 +0300

    react router try fix

--- src/main/java/com/couponmania2/coupon_project/facade/CustomerServiceImpl.java
+++ src/main/java/com/couponmania2/coupon_project/facade/CustomerServiceImpl.java
@@ -37,9 +37,11 @@ public class CustomerServiceImpl implements CustomerService {
     public Customer checkCredentials(String userName, String userPass, ClientType clientType) throws AppUnauthorizedRequestException {
         System.out.println("IM IN CUSTOMER SERVICE CHECK CREDENTIALS");
         if (customerRepo.findByEmailAndPassword(userName, userPass).isEmpty() || !(clientType.equals(ClientType.customer))) {
+            System.out.println("FAILED CHECK CREDENTIALS");
+            System.out.println(userName + " " + userPass);
+            System.out.println(clientType);
             throw new AppUnauthorizedRequestException(AppUnauthorizedRequestMessage.BAD_CREDENTIALS.getMessage());
         }
-        System.out.println("BEFORE RETURN CUSTOMER: " + userName + " " + userPass);
         return customerRepo.findByEmailAndPassword(userName, userPass).get();
     }
 
