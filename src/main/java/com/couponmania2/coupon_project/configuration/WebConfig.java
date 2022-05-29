0122e909dc63cb04a9471dc7f9503a3f5e9bc85e
tamboor <nirkatz20exc@gmail.com>
2022-05-30 00:47:21 +0300

    browserrouter + mvc config

--- src/main/java/com/couponmania2/coupon_project/configuration/WebConfig.java
+++ src/main/java/com/couponmania2/coupon_project/configuration/WebConfig.java
@@ -1,24 +1,25 @@
-//package com.couponmania2.coupon_project.configuration;
-////
-////import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.cors.CorsConfiguration;
-//import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-//import org.springframework.web.filter.CorsFilter;
-//import org.springframework.web.servlet.config.annotation.CorsRegistry;
-//import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-//import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+package com.couponmania2.coupon_project.configuration;
 //
-//@Configuration
-////@EnableWebMvc
-//public class WebConfig implements WebMvcConfigurer {
-//    @Override
-//    public void addViewControllers(ViewControllerRegistry registry) {
-//        registry.addViewController("/{spring:\\w+}")
-//                .setViewName("forward:/");
-//        registry.addViewController("/**/{spring:\\w+}")
-//                .setViewName("forward:/");
-//        registry.addViewController("/{spring:\\w+}/**{spring:?!(\\.js|\\.css)$}")
-//                .setViewName("forward:/");    }
-//}
+//import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+//@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/{spring:\\w+}")
+                .setViewName("forward:/");
+        registry.addViewController("/**/{spring:\\w+}")
+                .setViewName("forward:/");
+        registry.addViewController("/{spring:\\w+}/**{spring:?!(\\.js|\\.css)$}")
+                .setViewName("forward:/");
+    }
+}
